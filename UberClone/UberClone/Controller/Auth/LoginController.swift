f1fdc2895be462e0333af19ad5b506cef4081b87
Ahmet Hakan YILDIRIM <hakan-336-ahmet@outlook.com>
2025-02-25 23:04:29 +0300

    initialized project

---
+++ UberClone/UberClone/Controller/Auth/LoginController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginController.swift
+//  UberClone
+//
+//  Created by AHMET HAKAN YILDIRIM on 25.02.2025.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
